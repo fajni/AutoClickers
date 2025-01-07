0b778daa4702e1ad69255d16685028e0060e275e
fajni <keychiper.cy@gmail.com>
2025-01-08 00:17:37 +0100

    Added .jar files .jar files, note for generating .jar files

---
+++ jar/AutoClicker.bat
@@ -0,0 +1,6 @@
+@echo on
+
+REM cd /d E:\Programs\Java\Java Workspace\IntelliJ IDEA Workspace\AutoClicker\jar
+cd /d "%~dp0"
+
+start /min java -jar AutoClicker.jar
---
+++ jar/WASD.bat
@@ -0,0 +1,5 @@
+@echo on
+
+cd /d "%~dp0"
+
+start /min java -jar WASD.jar
